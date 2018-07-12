cde7cd83eed1ea611c48eabcf927833b896e3d2c
Yash <yashp241195@gmail.com>
2018-07-12 17:58:26 +0530

    Update display_rows.php

--- table/display_rows.php
+++ table/display_rows.php
@@ -6,9 +6,16 @@ header("Content-Type: application/json; charset=UTF-8");
 
 $Isadmin = $_SESSION['Isadmin'];
 
-if($_POST['t_name'] != Null){
+if($_POST['t_name'] != Null && $_POST['page_no'] != Null && $_POST['row_limit'] != Null){
 	
 	$t_name = $_POST['t_name'];
+	$page_no = $_POST['page_no'];
+	$row_limit = $_POST['row_limit'];
+
+	$offset = ($page_no-1)*$row_limit;
+
+	$total_row_count = 0;
+
 	$all_row = "";	
 
 
@@ -18,18 +25,38 @@ if($_POST['t_name'] != Null){
 
 		if($Isadmin==true){
 
+			$count_query = "SELECT COUNT(body_measurements_id) from body_measurements inner join member_users on 
+			body_measurements.user_id=member_users.member_id";
+
+			$count_query = mysqli_query($conn, $count_query) or die(mysqli_error($conn));
+			$row = mysqli_fetch_array($count_query);
+
+			$total_row_count = $row['COUNT(body_measurements_id)'];
+
 			$query = "SELECT * from body_measurements inner join member_users on 
-			body_measurements.user_id=member_users.member_id ORDER BY date DESC";
+			body_measurements.user_id=member_users.member_id ORDER BY date DESC LIMIT $offset,$row_limit";
+
+			$user_query = mysqli_query($conn, $query) or die(mysqli_error($conn));
+			$body_measurements = $user_query;
 
 		}else{
-			$uid = $_SESSION['id'];			
-			$query = "SELECT * from body_measurements WHERE user_id = $uid ORDER BY date DESC";
+
+			$uid = $_SESSION['id'];	
+
+			$count_query = "SELECT COUNT(body_measurements_id) from body_measurements WHERE user_id = $uid";			
+			
+			$count_query = mysqli_query($conn, $count_query) or die(mysqli_error($conn));
+			$row = mysqli_fetch_array($count_query);
+
+			$total_row_count = $row['COUNT(body_measurements_id)'];
+
+			$query = "SELECT * from body_measurements WHERE user_id = $uid ORDER BY date DESC LIMIT $offset,$row_limit";
+
+			$user_query = mysqli_query($conn, $query) or die(mysqli_error($conn));
+			$body_measurements = $user_query;
 
 		}
 
-    
-		$user_query = mysqli_query($conn, $query) or die(mysqli_error($conn));
-		$body_measurements = $user_query;
 		
 
 		while ($row = mysqli_fetch_array($body_measurements)){
@@ -80,9 +107,9 @@ if($_POST['t_name'] != Null){
 
 	}
 
-
+	$my_array = array($all_row,$total_row_count);
 	
-	echo $all_row;
+	echo json_encode($my_array);
 
 
 }

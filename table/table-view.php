d747007dfe6d7ff194b701e2501d32d7bbf933c7
Yash <yashp241195@gmail.com>
2018-07-12 18:02:19 +0530

    Update table-view.php

--- table/table-view.php
+++ table/table-view.php
@@ -1,29 +1,39 @@
-<table id="body_measurements" style="font-size:12px" class="table table-striped table-bordered table-list">
-    <thead>
-        <tr>
-            <th>Date </th>
-            <?php if($Isadmin){ echo "<th>Member Email</th>"; } ?>
-            <th>Height </th>
-            <th>Weight</th>
-            <th>Upper Abs</th>
-            <th>Middle Abs</th>
-            <th>Lower Abs</th>
-            <th>Hips</th>
-            <th>Thighs</th>
-            <th>Left Arm</th>
-            <th>Right Arm</th>
-            <?php if($Isadmin==false){ echo "<th>Action</th>"; } ?>
-        </tr>
-    </thead>
+<div class="panel-body">
+    <table id="body_measurements" style="font-size:12px" class="table table-striped table-bordered table-list">
+        <thead>
+            <tr>
+                <th>Date </th>
+                <?php if($Isadmin){ echo "<th>Member Email</th>"; } ?>
+                <th>Height </th>
+                <th>Weight</th>
+                <th>Upper Abs</th>
+                <th>Middle Abs</th>
+                <th>Lower Abs</th>
+                <th>Hips</th>
+                <th>Thighs</th>
+                <th>Left Arm</th>
+                <th>Right Arm</th>
+                <?php if($Isadmin==false){ echo "<th>Action</th>"; } ?>
+            </tr>
+        </thead>
 
-    <tbody id="body_measurements_rows">                          
-    </tbody>
+        <tbody id="body_measurements_rows">                          
+        </tbody>
 
-</table>
+    </table>
 
+</div>
+<div class="panel-footer">
+    <div class="row">
+        <div class="col col-xs-4" id="body_measurements_page_no">
+            <p>Page 1 of 5</p>
+        </div>
 
-<script type="text/javascript" src="ajax_js/display_rows.js">
-</script>
+        <div class="col col-xs-8">
+            <ul id="body_measurements_pagination" class="pagination hidden-xs pull-right">
+            </ul>
+        </div>
 
-<script type="text/javascript" src="ajax_js/fill_table_details.js">
-</script>
+    </div>
+</div>
+

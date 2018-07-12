2728dc7196d3af52d19c58a562202900abd32917
Yash <yashp241195@gmail.com>
2018-07-12 18:07:22 +0530

    Update table-view.php

--- table/table-view.php
+++ table/table-view.php
@@ -1,39 +1,62 @@
-<div class="panel-body">
-    <table id="body_measurements" style="font-size:12px" class="table table-striped table-bordered table-list">
-        <thead>
-            <tr>
-                <th>Date </th>
-                <?php if($Isadmin){ echo "<th>Member Email</th>"; } ?>
-                <th>Height </th>
-                <th>Weight</th>
-                <th>Upper Abs</th>
-                <th>Middle Abs</th>
-                <th>Lower Abs</th>
-                <th>Hips</th>
-                <th>Thighs</th>
-                <th>Left Arm</th>
-                <th>Right Arm</th>
-                <?php if($Isadmin==false){ echo "<th>Action</th>"; } ?>
-            </tr>
-        </thead>
+<!-- table inside panel -->
+<div class="panel panel-default panel-table" style="overflow-x:auto;">
+    <div class="panel-heading">
+        <div class="row">
+            <div class="col col-xs-6">
+                <h3 class="panel-title">Body Measurements</h3>
+            </div>
+            <div class="col col-xs-6 text-right">
+            <?php if($Isadmin==false && $fee_form_enable == false){ ?>
+            <button onclick="refresh_table()" class=" btn btn-sm btn-success glyphicon glyphicon-refresh  
+                "></button>
 
-        <tbody id="body_measurements_rows">                          
-        </tbody>
+            <button type="button" 
+            onclick="insert_row('body_measurements')" 
 
-    </table>
+            class="btn btn-sm 
+            btn-primary btn-create">Create New</button>
+            <?php } ?>
+            </div>
 
-</div>
-<div class="panel-footer">
-    <div class="row">
-        <div class="col col-xs-4" id="body_measurements_page_no">
-            <p>Page 1 of 5</p>
         </div>
+    </div>
 
-        <div class="col col-xs-8">
-            <ul id="body_measurements_pagination" class="pagination hidden-xs pull-right">
-            </ul>
-        </div>
+    <div class="panel-body">
+        <table id="body_measurements" style="font-size:12px" class="table table-striped table-bordered table-list">
+            <thead>
+                <tr>
+                    <th>Date </th>
+                    <?php if($Isadmin){ echo "<th>Member Email</th>"; } ?>
+                    <th>Height </th>
+                    <th>Weight</th>
+                    <th>Upper Abs</th>
+                    <th>Middle Abs</th>
+                    <th>Lower Abs</th>
+                    <th>Hips</th>
+                    <th>Thighs</th>
+                    <th>Left Arm</th>
+                    <th>Right Arm</th>
+                    <?php if($Isadmin==false){ echo "<th>Action</th>"; } ?>
+                </tr>
+            </thead>
+
+            <tbody id="body_measurements_rows">                          
+            </tbody>
+
+        </table>
 
     </div>
-</div>
+    <div class="panel-footer">
+        <div class="row">
+            <div class="col col-xs-4" id="body_measurements_page_no">
+                <p>Page 1 of 5</p>
+            </div>
 
+            <div class="col col-xs-8">
+                <ul id="body_measurements_pagination" class="pagination hidden-xs pull-right">
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</div>
